5dc1433fbdbbe7d47340c021f6b99d7787d231dd
Homero Raul <h.raulvc@gmail.com>
2024-06-16 08:34:53 -0600

    Update CustomerTrait.php

--- src/Traits/CustomerTrait.php
+++ src/Traits/CustomerTrait.php
@@ -36,6 +36,23 @@ trait CustomerTrait
         return new CustomerResponse($response->json());
     }
 
+    public function createCustomerPortalSession(string $customerId): string
+    {
+        $response = Http::withBasicAuth($this->token, '')
+            ->asForm()
+            ->post($this->getUrl('/v1/billing_portal/sessions'), [
+                'customer' => $customerId,
+                'return_url' => config('omnibilling.processors_config.stripe.portal_return_url'), 
+            ]);
+
+        if ($response->failed()) {
+            dd($response->json());
+            throw new \Exception('Failed to create customer portal session');
+        }
+
+        return $response->json()['url'];
+    }
+
     public function addPaymentMethodToCustomer($customerId, array $paymentMethod)
     {
         dd('Stripe: addPaymentMethodToCustomer');
